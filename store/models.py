f9024faee7d5eb954ecfa34ff15794f26f063f3a
nikhil-acuver <125883162+nikhil-acuver@users.noreply.github.com>
2023-03-13 23:00:22 +0530

    Creating models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True) #auto_now will automatically save the current d/t field
+    
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=50)
+    birth_date = models.DateField(null=True)
+        
